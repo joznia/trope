15984b78435ac7d950b1018c7ff01c055700f79b
chris b <mail@joznia.com>
2021-05-03 17:23:19 -0700

    Fix install script

--- install.pl
+++ install.pl
@@ -8,8 +8,9 @@ $dest = "/usr/bin/space";
 $a = $ARGV[0];
 if ($a eq '-u') {
     if (-e $dest) {
-        unlink $dest;
+        unlink $dest or die "uninstall failed: $! \n";
     }
 } else {
     copy $src, $dest or die "install failed: $! \n";
+    chmod 0755, $dest;
 }
